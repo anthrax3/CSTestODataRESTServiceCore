424894f5093db51c659e52c78c9fd9cd3c7911aa
d7p4n4 <bercikezs199999999@gmail.com>
2020-08-19 16:45:01 +0200

    1.20200819.2.bzs

--- CSTestODataRESTServiceCore/Program.cs
+++ CSTestODataRESTServiceCore/Program.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -16,18 +15,18 @@ namespace CSTestODataRESTServiceCore
         {
             CreateHostBuilder(args).Build().Run();
         }
-        /*
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });*/
-
+                });
+        /*
         public static IWebHostBuilder CreateHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-            .UseUrls("http://89.46.75.137:8087")
+            .UseUrls("http://89.46.75.137:8888")
             .UseKestrel()
-            .UseStartup<Startup>();
+            .UseStartup<Startup>();*/
     } //Program
 }
